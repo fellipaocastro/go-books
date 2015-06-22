03199c64179eea313444ffc5c613ad351053346e
Fellipe Castro <contact@fellipecastro.com>
2015-06-22 12:52:13 -0300

    Update

--- main.go
+++ main.go
@@ -1,57 +1,57 @@
 package main
 
 import (
-    "os"
-    "github.com/codegangsta/martini"
-    "github.com/martini-contrib/render"
-    "net/http"
-    "database/sql"
-    _ "github.com/lib/pq"
+	"database/sql"
+	"github.com/codegangsta/martini"
+	_ "github.com/lib/pq"
+	"github.com/martini-contrib/render"
+	"net/http"
+	"os"
 )
 
 type Book struct {
-    Title       string
-    Author      string
-    Description string
+	Title       string
+	Author      string
+	Description string
 }
 
 func SetupDB() *sql.DB {
-    dbUrl := os.Getenv("DATABASE_URL")
-    if len(dbUrl) == 0 {
-        dbUrl = "postgres://postgres:postgres@127.0.0.1:5432/go-books?sslmode=disable"
-    }
+	dbUrl := os.Getenv("DATABASE_URL")
+	if len(dbUrl) == 0 {
+		dbUrl = "postgres://postgres:postgres@127.0.0.1:5432/go-books?sslmode=disable"
+	}
 
-    db, err := sql.Open("postgres", dbUrl)
-    PanicIf(err)
-    return db
+	db, err := sql.Open("postgres", dbUrl)
+	PanicIf(err)
+	return db
 }
 
 func PanicIf(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-    m := martini.Classic()
-    m.Map(SetupDB())
-    m.Use(render.Renderer(render.Options{
-        Layout: "layout",
-    }))
+	m := martini.Classic()
+	m.Map(SetupDB())
+	m.Use(render.Renderer(render.Options{
+		Layout: "layout",
+	}))
 
-    m.Get("/", ShowBooks) 
-    m.Get("/create", NewBook)
-    m.Post("/books", Create)
+	m.Get("/", ShowBooks)
+	m.Get("/create", NewBook)
+	m.Post("/books", Create)
 
-    m.Run()
+	m.Run()
 }
 
 func NewBook(ren render.Render) {
-    ren.HTML(200, "create", nil)
+	ren.HTML(200, "create", nil)
 }
 
 func Create(ren render.Render, r *http.Request, db *sql.DB) {
-    var sql string = `
+	var sql string = `
         INSERT INTO books (
             title,
             author,
@@ -61,22 +61,22 @@ func Create(ren render.Render, r *http.Request, db *sql.DB) {
             $2,
             $3);`
 
-    rows, err := db.Query(
-        sql,
-        r.FormValue("title"),
-        r.FormValue("author"),
-        r.FormValue("description"))
+	rows, err := db.Query(
+		sql,
+		r.FormValue("title"),
+		r.FormValue("author"),
+		r.FormValue("description"))
 
-    PanicIf(err)
-    defer rows.Close()
+	PanicIf(err)
+	defer rows.Close()
 
-    ren.Redirect("/")
+	ren.Redirect("/")
 }
 
 func ShowBooks(ren render.Render, r *http.Request, db *sql.DB) {
-    searchTerm := "%" + r.URL.Query().Get("q") + "%"
+	searchTerm := "%" + r.URL.Query().Get("q") + "%"
 
-    var sql string = `
+	var sql string = `
         SELECT
             title,
             author,
@@ -88,18 +88,17 @@ func ShowBooks(ren render.Render, r *http.Request, db *sql.DB) {
             OR author ILIKE $1
             OR description ILIKE $1;`
 
-    rows, err := db.Query(sql, searchTerm)
-    PanicIf(err)
-    defer rows.Close()
+	rows, err := db.Query(sql, searchTerm)
+	PanicIf(err)
+	defer rows.Close()
 
-    books := []Book{}
-    for rows.Next() {
-        b := Book{}
-        err := rows.Scan(&b.Title, &b.Author, &b.Description)
-        PanicIf(err)
-        books = append(books, b)
-    }
+	books := []Book{}
+	for rows.Next() {
+		b := Book{}
+		err := rows.Scan(&b.Title, &b.Author, &b.Description)
+		PanicIf(err)
+		books = append(books, b)
+	}
 
-    ren.HTML(200, "books", books)
+	ren.HTML(200, "books", books)
 }
-
